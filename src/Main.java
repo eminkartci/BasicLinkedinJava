3b2763cb925a08e87e289be211307c612f60e679
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-26 21:25:40 +0300

    Welcome panel

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,31 @@
 
 class Main{
 
+    public String version = "V.1.1";
+    public String[] devs = {"Emin Kartci","Cem Özkul"};
+
     public static void main(String[] args) {
         
+        Main app = new Main();
+        app.welcome();
+
     }
+
+    public void welcome(){
+        
+        String content =    "|------------------------------|"+ "\n"+
+                            "|--- Welcome Basic Linkedin ---|"+ "\n"+
+                            "| Version   : " + version +          "\n";
+        
+        for(String x : devs){
+            content += "| Developer : " + x +"\n";
+        }
+
+        content += "|------------------------------|"+ "\n";
+
+        System.out.println(content);
+    }
+
+    
 
 }
