2f25aa2149edfc87989a2c29db47191c5f14045a
eminkartci <emin.kartci@ozu.edu.tr>
2021-09-05 23:08:38 +0300

    txt file can be read

--- src/User.java
+++ src/User.java
@@ -1,22 +1,45 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class User {
 
     // CONSTANTS
-    String USERS_PATH = "src/users.txt";
+    static String USERS_PATH = "src/users.txt";
 
     // MAIN
     public static void main(String[] args) {
         // User Instance
-        User user1 = new User(0,"eminkartci","123123");
-        User user2 = new User(1,"cemozkul","321321");
-        System.out.println(user1);
-        System.out.println(user2);
+        // User user1 = new User(0,"eminkartci","123123");
+        // User user2 = new User(1,"cemozkul","321321");
+        // System.out.println(user1);
+        // System.out.println(user2);
+
+        User.load_users();
+
     }
 
     // STATIC METHODS
     public static void load_users(){
 
+        try {
+            
+            BufferedReader br = new BufferedReader(new FileReader(new File(USERS_PATH)));
+            String line = "";
 
+            while((line = br.readLine()) != null ){
+                System.out.println(line);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
     
